ec2c6d71d954cf72c823c3bf0ad0da7fda26c736
CraigBaron <40808671+CraigBaron@users.noreply.github.com>
2021-05-26 21:37:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build")); // change this if your dir structure is different
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
