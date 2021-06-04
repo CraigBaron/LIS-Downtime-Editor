bd7ba5ea6ba6f48227b3e288776e90397d7722c0
beccakiz <60019378+beccakiz@users.noreply.github.com>
2021-06-04 13:18:24 -0400

    Update machineRecords.js

--- backend/routes/machineRecords.js
+++ backend/routes/machineRecords.js
@@ -4,18 +4,144 @@ const sql = require('mssql');
 
 const verifyAuthToken = require("../middleware/authenticate");
 
-
+//set templinenumber to case of table you want 
+var templinenumber = 11;
 
 router.get('/', async (req, res) =>
 {
-    var request = new sql.Request();
+    
+    var request = new sql.Request(); 
     // query to the database and get the records
-   request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line2 System 1]", function (err, recordset) {
-        if (err) console.log(err)
+   switch (templinenumber) {
 
-        res.json(recordset)        
+       //system 1 line 1
+        case 11:
+            
+            request.query("SELECT UniqueID, pkDowntimeEventID_NO, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line1 System 1]", function (err, recordset) {
+                if (err) console.log(err)
 
-    });
+                res.json(recordset)     
+            });  
+            break;
+
+        //system 1 line 2
+        case 12:
+            request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line2 System 1]", function (err, recordset) {
+                if (err) console.log(err)
+
+                res.json(recordset)        
+            }); 
+            break;
+
+        //system 1 line 3
+        case 13:
+            request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line3 System 1]", function (err, recordset) {
+                if (err) console.log(err)
+       
+                res.json(recordset)        
+            }); 
+            break;
+
+        //system 1 line 4
+        case 14:
+            request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line4 System 1]", function (err, recordset) {
+                if (err) console.log(err)
+           
+                res.json(recordset)        
+            }); 
+            break;
+
+        //system 1 line 8
+        case 15:
+            request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line5 System 1]", function (err, recordset) {
+                if (err) console.log(err)
+           
+                res.json(recordset)        
+            }); 
+            request.query("UPDATE [DowntimeEvents_Line5 System 1] SET LineID = '8' WHERE LineID = '5'", function (err, recordset) {
+                if (err) console.log(err)
+           
+                console.log(recordset.rowsAffected + " records updated");      
+            }); 
+            break;   
+
+        //system 2 line 4
+        case 21:
+            request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line1 System 2]", function (err, recordset) {
+                if (err) console.log(err)
+               
+                res.json(recordset)        
+            }); 
+            request.query("UPDATE [DowntimeEvents_Line1 System 2] SET LineID = '4' WHERE LineID = '1'", function (err, recordset) {
+                if (err) console.log(err)
+               
+                console.log(recordset.rowsAffected + " records updated");      
+            }); 
+            break;   
+
+        //system 2 line 5
+        case 22:
+            request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line2 System 2]", function (err, recordset) {
+                if (err) console.log(err)
+                   
+                res.json(recordset)        
+            }); 
+            request.query("UPDATE [DowntimeEvents_Line2 System 2] SET LineID = '5' WHERE LineID = '2'", function (err, recordset) {
+                if (err) console.log(err)
+                   
+                console.log(recordset.rowsAffected + " records updated");      
+            }); 
+            break;   
+
+        //system 2 line 7
+        case 23:
+            request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line3 System 2]", function (err, recordset) {
+                if (err) console.log(err)
+                       
+                res.json(recordset)        
+            }); 
+            request.query("UPDATE [DowntimeEvents_Line3 System 2] SET LineID = '7' WHERE LineID = '3'", function (err, recordset) {
+                if (err) console.log(err)
+                       
+                console.log(recordset.rowsAffected + " records updated");      
+            }); 
+            break;  
+
+        //system 2 line 9
+        case 24:
+            request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line4 System 2]", function (err, recordset) {
+                if (err) console.log(err)
+                           
+                res.json(recordset)        
+            }); 
+            request.query("UPDATE [DowntimeEvents_Line4 System 2] SET LineID = '9' WHERE LineID = '4'", function (err, recordset) {
+                if (err) console.log(err)
+                           
+                console.log(recordset.rowsAffected + " records updated");      
+            }); 
+            break; 
+
+        //system 2 line 6
+        case 25:
+            request.query("SELECT UniqueID, pkDowntimeEventID, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line5 System 2]", function (err, recordset) {
+                if (err) console.log(err)
+                               
+                res.json(recordset)        
+            }); 
+            request.query("UPDATE [DowntimeEvents_Line5 System 2] SET LineID = '6' WHERE LineID = '5'", function (err, recordset) {
+                if (err) console.log(err)
+                               
+                console.log(recordset.rowsAffected + " records updated");      
+            }); 
+            break; 
+        
+        default:
+            request.query("SELECT UniqueID, pkDowntimeEventID_NO, StartDateTime, EndDateTime, DurationTotalMinutes, LineID, Machine, ComponentID, Comments, Secondarypk FROM [DowntimeEvents_Line1 System 1]", function (err, recordset) {
+                if (err) console.log(err)
+
+                res.json(recordset)     
+            }); 
+}
 });
 
 
